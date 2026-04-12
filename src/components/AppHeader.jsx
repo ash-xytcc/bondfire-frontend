ce3296febb961b01922dd22fdce4c4f91623ba32
ash.budding <bjgarr@gmail.com>
2026-04-12 06:34:00 +0000

    ddwdw

--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -84,7 +84,7 @@ const Brand = ({ orgId, logoSrc }) => {
   return (
     <div className="bf-brand-wrap">
       <Link className="bf-brand" to={homeHref}>
-        {imgSrc ? <img src={imgSrc} alt="Org logo" onError={(e)=>{ e.currentTarget.style.display = "none"; }} /> : null}
+        <img src={imgSrc} alt="Red Harbor logo" className="bf-org-logo" />
         <span>{label}</span>
       </Link>
 

287f5aeb247c82affb100f34052b13ccec402cb0
ash-xytcc <ash-xytcc@github.com>
2026-04-24 00:06:48 -0700

    fix router provider for public app shell

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
+import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import "./debug/initDebug.js";
 
@@ -30,6 +31,8 @@ try {
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );

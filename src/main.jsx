28d83da8ec519ed5270fb300b549f7e499cce1f3
ash.budding <bjgarr@gmail.com>
2026-09-11 12:42:27 -0700

    Load Bondfire V3 global theme

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import "./v3-theme.css";
 import "./debug/initDebug.js";
 
 // Temporary hard reset of stale PWA/service-worker state.
